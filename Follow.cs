2e53f686397e21ffa513b045b500bdaecdebe238
JongBin Jo <jbs32_1317@js.hs.kr>
2022-07-12 08:43:34 +0900

    Unity3DGame_CameraMove

---
+++ Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    //설정한 카메라 위치(3인칭)의 구도로 플레이어를 따라가게 하는 오프셋
+    public float delaytime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * 15 *delaytime );
+    }
+}
